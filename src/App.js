3421e1a0e8e8003a60aafcc188cb628cd20cd28c
Arseniy Boltzmunn <ars.boltz@gmail.com>
2022-03-13 12:33:51 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import React, {useState} from "react";
 
 
-function compInitialCounter() {
-    return Math.trunc(Math.random()*20)
-}
-
 function App() {
 
-    const [counter, setCounter] = useState(compInitialCounter())
 
+    const [counter, setCounter] = useState(0)
 
     function increment() {
-        // setCounter(counter + 1)
-        // setCounter(counter + 1)
-        setCounter((prevCounter) => {
-return prevCounter + 1
-        })
-        setCounter( prev => prev +1)
+        setCounter(counter +1)
+    }
+    function decrement() {
+        setCounter(counter -1)
     }
 
-    function decrement() {
-        setCounter(counter - 1)
-    }
 
     return (
         <div>
-            <h1>Cчетчик: {counter}</h1>
-            <button onClick={increment}>Add</button>
-            <button onClick={decrement}>delete</button>
+            <h1>Data {counter}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
-
-export default App;
+export default App
